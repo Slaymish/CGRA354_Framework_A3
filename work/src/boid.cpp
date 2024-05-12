4b6ef29dd97b99fcd8953ca0da93ef5ab1bddf3c
Hamish <hamishapps@gmail.com>
2024-05-12 18:18:27 +1200

    refactor: Improve boid calculations for alignment, cohesion, and avoidance

--- work/src/boid.cpp
+++ work/src/boid.cpp
@@ -53,12 +53,10 @@ void Boid::calculateForces(Scene *scene) {
   for (const Boid &other : scene->boids()) {
     if (&other != this && glm::distance(other.position(), this->position()) <
                               scene->boidRadius()) {
-      average_velocity += other.velocity(); // for alignment
-      average_position += other.position(); // for cohesion
-      avoidance +=
-          (this->position() - other.position()) /
-          glm::distance(this->position(), other.position()); // for
-                                                             // avoidance
+      average_velocity += glm::normalize(other.velocity()); // for alignment
+      average_position += other.position();                 // for cohesion
+      avoidance += (this->position() - other.position()) /
+                   glm::distance(other.position(), this->position());
       neighbours++;
     }
   }
@@ -68,15 +66,18 @@ void Boid::calculateForces(Scene *scene) {
     average_position /= neighbours;
 
     // Alignment
-    glm::vec3 alignment =
-        (average_velocity - this->velocity()) * scene->alignmentWeight();
+    glm::vec3 alignment = (average_velocity - m_velocity);
+
+    alignment = glm::normalize(alignment) * scene->alignmentWeight();
 
     // Cohesion
-    glm::vec3 cohesion =
-        (average_position - this->position()) * scene->cohesionWeight();
+    glm::vec3 cohesion = (average_position - this->position());
+
+    cohesion = glm::normalize(cohesion) * scene->cohesionWeight();
 
     // Avoidance
-    glm::vec3 avoidanceForce = avoidance * scene->avoidanceWeight();
+    glm::vec3 avoidanceForce =
+        glm::normalize(avoidance) * scene->avoidanceWeight();
 
     m_acceleration += alignment + cohesion + avoidanceForce;
   }
@@ -87,19 +88,22 @@ void Boid::calculateForces(Scene *scene) {
   glm::vec3 soft_bound_force = glm::vec3(0);
 
   if (m_position.x < -bound.x + soft_bound)
-    soft_bound_force.x = 1;
+    soft_bound_force.x = (-bound.x + soft_bound - m_position.x);
   if (m_position.x > bound.x - soft_bound)
-    soft_bound_force.x = -1;
+    soft_bound_force.x = (bound.x - soft_bound - m_position.x);
   if (m_position.y < -bound.y + soft_bound)
-    soft_bound_force.y = 1;
+    soft_bound_force.y = (-bound.y + soft_bound - m_position.y);
   if (m_position.y > bound.y - soft_bound)
-    soft_bound_force.y = -1;
+    soft_bound_force.y = (bound.y - soft_bound - m_position.y);
   if (m_position.z < -bound.z + soft_bound)
-    soft_bound_force.z = 1;
+    soft_bound_force.z = (-bound.z + soft_bound - m_position.z);
   if (m_position.z > bound.z - soft_bound)
-    soft_bound_force.z = -1;
+    soft_bound_force.z = (bound.z - soft_bound - m_position.z);
 
-  m_acceleration += soft_bound_force;
+  soft_bound_force = glm::normalize(soft_bound_force) * 0.1f;
+
+  if (glm::length(soft_bound_force) > 0)
+    m_acceleration += soft_bound_force;
 }
 
 void Boid::update(float timestep, Scene *scene) {
@@ -112,23 +116,55 @@ void Boid::update(float timestep, Scene *scene) {
   // change the position (if wrapping).
   //-------------------------------------------------------------
 
-  // update velocity
-  m_velocity += m_acceleration * timestep;
-
   // Limit speed
   if (glm::length(m_velocity) > scene->maxSpeed()) {
     m_velocity = glm::normalize(m_velocity) * scene->maxSpeed();
+  } else if (glm::length(m_velocity) < scene->minSpeed()) {
+    m_velocity = glm::normalize(m_velocity) * scene->minSpeed();
   }
+
+  // update velocity
+  m_velocity += m_acceleration * timestep;
 
   // update position
   m_position += m_velocity * timestep;
 
   // Bounce at the walls
-  glm::vec3 bound = scene->bound();
-  if (m_position.x < -bound.x || m_position.x > bound.x)
-    m_velocity.x = -m_velocity.x;
-  if (m_position.y < -bound.y || m_position.y > bound.y)
-    m_velocity.y = -m_velocity.y;
-  if (m_position.z < -bound.z || m_position.z > bound.z)
-    m_velocity.z = -m_velocity.z;
+  bool bounce = scene->bounce();
+  if (bounce) {
+    glm::vec3 bound = scene->bound(); // half-size of the bounding box
+    if (m_position.x < -bound.x || m_position.x > bound.x) {
+      m_velocity.x *= -1;
+    }
+    if (m_position.y < -bound.y || m_position.y > bound.y) {
+      m_velocity.y *= -1;
+    }
+    if (m_position.z < -bound.z || m_position.z > bound.z) {
+      m_velocity.z *= -1;
+    }
+  }
+
+  // Wrap around the walls
+  bool wrap = scene->wrap();
+  if (wrap) {
+    glm::vec3 bound = scene->bound();
+    if (m_position.x < -bound.x) {
+      m_position.x = bound.x;
+    }
+    if (m_position.x > bound.x) {
+      m_position.x = -bound.x;
+    }
+    if (m_position.y < -bound.y) {
+      m_position.y = bound.y;
+    }
+    if (m_position.y > bound.y) {
+      m_position.y = -bound.y;
+    }
+    if (m_position.z < -bound.z) {
+      m_position.z = bound.z;
+    }
+    if (m_position.z > bound.z) {
+      m_position.z = -bound.z;
+    }
+  }
 }
